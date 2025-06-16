ea6aad2ba347a42a0de4a1ccb4b6174a78688d39
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-06-16 22:38:38 +0800

    chore: update version headers and manifest files for various language bindings and test applications (#983)

--- core/include_internal/ten_runtime/common/version.h
+++ core/include_internal/ten_runtime/common/version.h
@@ -6,4 +6,4 @@
 //
 #pragma once
 
-#define TEN_RUNTIME_VERSION "0.10.13"
+#define TEN_RUNTIME_VERSION "0.10.14"
--- core/src/ten_manager/src/version.rs
+++ core/src/ten_manager/src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "0.10.13";
+pub const VERSION: &str = "0.10.14";
