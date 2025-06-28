ace059581fdb49739a98cd473c12c456fdf2681b
xxxxl_sun <31622273+sunxilin@users.noreply.github.com>
2025-06-29 00:59:29 +0800

    feat: add more test cases (#1039)

---
+++ tests/ten_runtime/integration/go/set_dest_go/set_dest_go_app/main.go
@@ -0,0 +1,61 @@
+//
+// Copyright © 2025 Agora
+// This file is part of TEN Framework, an open source project.
+// Licensed under the Apache License, Version 2.0, with certain conditions.
+// Refer to the "LICENSE" file in the root directory for more information.
+//
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+	"time"
+
+	ten "ten_framework/ten_runtime"
+)
+
+type defaultApp struct {
+	ten.DefaultApp
+}
+
+func (p *defaultApp) OnInit(tenEnv ten.TenEnv) {
+	tenEnv.LogDebug("onInit")
+	tenEnv.OnInitDone()
+}
+
+func (p *defaultApp) OnDeinit(tenEnv ten.TenEnv) {
+	tenEnv.LogDebug("onDeinit")
+	tenEnv.OnDeinitDone()
+}
+
+func main() {
+	// test app
+	app, err := ten.NewApp(&defaultApp{})
+	if err != nil {
+		fmt.Println("Failed to create app.")
+	}
+
+	app.Run(true)
+	app.Wait()
+
+	// A single GC is not enough; multiple rounds of GC are needed to clean up
+	// as thoroughly as possible.
+	//
+	// Note: Because the ten-runtime's own leak check mechanism is enabled
+	// during testing, we still need the following multiple GC calls to actually
+	// trigger the Go layer's finalizers to avoid a situation where Go
+	// finalizers are not called before the entire process exits, which would
+	// cause what could be considered a real memory leak to some extent.
+	for i := 0; i < 10; i++ {
+		// Explicitly trigger GC to increase the likelihood of finalizer
+		// execution.
+		debug.FreeOSMemory()
+		runtime.GC()
+
+		// Wait for a short period to give the GC time to run.
+		runtime.Gosched()
+		time.Sleep(1 * time.Second)
+	}
+}
