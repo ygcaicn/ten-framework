59761310cc5a310e71dd78838b4d6550d7391d1f
plutoless <zhangqianze@agora.io>
2025-08-07 16:48:39 +0800

    fix: temp fix for task use error

--- ai_agents/agents/scripts/install_deps_and_build.sh
+++ ai_agents/agents/scripts/install_deps_and_build.sh
@@ -131,7 +131,7 @@ main() {
 
   # Install all dependencies specified in manifest.json.
   echo "install dependencies..."
-  tman --verbose install
+  tman --verbose install --max-latest-versions=100
 
   # build extensions and app
   echo "build_cxx_extensions..."
