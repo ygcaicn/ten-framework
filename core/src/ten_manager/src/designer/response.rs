e6fd91665ec84fc8d7fa3846677b8a29d38ccaf6
xxxxl_sun <31622273+sunxilin@users.noreply.github.com>
2025-08-21 18:31:18 +0800

    fix: get_graphs_endpoint (#1308)
    Co-authored-by: Hu Yueh-Wei <wei.hu.tw@gmail.com>

--- core/src/ten_manager/src/designer/response.rs
+++ core/src/ten_manager/src/designer/response.rs
@@ -4,6 +4,9 @@
 // Licensed under the Apache License, Version 2.0, with certain conditions.
 // Refer to the "LICENSE" file in the root directory for more information.
 //
+use std::fmt;
+
+use actix_web::{HttpResponse, ResponseError};
 use anyhow::{Error, Result};
 use serde::{Deserialize, Serialize};
 
@@ -92,5 +95,23 @@ impl ErrorResponse {
         }
 
         this
+    }
+}
+
+impl fmt::Display for ErrorResponse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl ResponseError for ErrorResponse {
+    fn error_response(&self) -> HttpResponse {
+        let error_response = ErrorResponse {
+            status: Status::Fail,
+            message: self.to_string(),
+            error: None,
+        };
+
+        HttpResponse::BadRequest().json(error_response)
     }
 }
